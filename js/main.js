21cc40ec321b8ce2c06bd554c08a68699db4a9b9
icharletes <cflorejo.carlos@gmail.com>
2017-07-29 21:34:47 -0400

    commit 50%

---
+++ js/main.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+     new WOW().init();
+
+
+//slider
+  $("#slides").slidesjs({
+		pagination: {
+			
+			effect: "slide",
+			navigation: false
+		}
+  });
+
+
+});     
+
+
